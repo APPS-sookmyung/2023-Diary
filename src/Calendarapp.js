a81c3f47bc0cfa1f26142bb8051c9f3019a664d4
gustj3104 <gustj7038@gmail.com>
2023-11-29 16:54:40 +0900

    Feat: 운동일지
    이모지 달력 반영

--- src/Calendarapp.js
+++ src/Calendarapp.js
@@ -1,12 +1,18 @@
-// 현재 날짜를 불러와서 date 객체 생성
 let date = new Date();
-// 달력 렌더링 함수
+
+function getCurrentDate() {
+  var today = new Date();
+  var year = today.getFullYear();
+  var month = String(today.getMonth() + 1).padStart(2, "0");
+  var day = String(today.getDate()).padStart(2, "0");
+  return year + "-" + month + "-" + day;
+}
 const renderCalendar = () => {
-  //현재 연도, 월 불러오기
+  //달력 함수
   const viewYear = date.getFullYear();
   const viewMonth = date.getMonth();
 
-  //year, month 클래스 요소에 연도, 월 표시
+  //현재 연도, 월
   document.querySelector(".year-month").textContent = `${viewYear}년 ${
     viewMonth + 1
   }월`;
@@ -15,35 +21,31 @@ const renderCalendar = () => {
   const prevLast = new Date(viewYear, viewMonth, 0); //1231
   const thisLast = new Date(viewYear, viewMonth + 1, 0); //0131
 
-  // 이전 달의 마지막 날 요일 저장
   const PLDate = prevLast.getDate();
   const PLDay = prevLast.getDay();
-  // 이번 달의 마지막 요일 저장
+
   const TLDate = thisLast.getDate();
   const TLDay = thisLast.getDay();
-  // 이번 달 날짜 배열 생성
+
   const prevDates = [];
   const thisDates = [...Array(TLDate + 1).keys()].slice(1, TLDate + 1);
   const nextDates = [];
 
   console.log(thisDates);
-  // 이전 달의 마지막 요일이 토요일이 아닐 때 이전 달의 날짜를 배열에 추가
   if (PLDay !== 6) {
     for (let i = 0; i < PLDay + 1; i++) {
       prevDates.unshift(PLDate - i);
     }
   }
-  //다음 달 날짜 배열에 추가
+
   for (let i = 1; i < 7 - TLDay; i++) {
     nextDates.push(i);
   }
 
-  //날짜 배열 합치기, 이번 달 첫날, 마지막 날 인덱스 구하기
   const dates = prevDates.concat(thisDates, nextDates);
   const firstDateIndex = dates.indexOf(1);
   const lastDateIndex = dates.lastIndexOf(TLDate);
 
-  //날짜 배열에서 현재 달, 다른 달 구분해서 HTML로 변환
   dates.forEach((date, i) => {
     const condition =
       i >= firstDateIndex && i < lastDateIndex + 1 ? "this" : "other";
@@ -51,21 +53,23 @@ const renderCalendar = () => {
       i
     ] = `<div class="date"><span class=${condition}>${date}</span></div>`;
   });
-  // dates 클래스 요소에 날짜 삽입
   document.querySelector(".dates").innerHTML = dates.join("");
-  // 로컬 스토리지에서 DiaryData 데이터 가져오기
-  const storedIcon = localStorage.getItem("DiaryData");
-  const parsedIcon = storedIcon ? JSON.parse(storedIcon).icon : null;
-  // 오늘 날짜 불러오기
-  const today = new Date();
-  // 현재 달이 오늘의 달과 일치하면 오늘 날짜에 today 클래스, 아이콘 추가
-  if (viewMonth === today.getMonth() && viewYear === today.getFullYear()) {
-    for (let date of document.querySelectorAll(".this")) {
-      if (+date.innerText === today.getDate()) {
-        date.classList.add("today");
 
-        if (parsedIcon) {
-          date.innerHTML += `<span class="icon">${parsedIcon}</span>`;
+  const today = new Date();
+  const Icon = getLatestIcon();
+
+  if (viewMonth === today.getMonth() && viewYear === today.getFullYear()) {
+    for (let dateCell of document.querySelectorAll(".this")) {
+      const dateNumber = parseInt(dateCell.innerText);
+
+      if (dateNumber === today.getDate()) {
+        dateCell.classList.add("today");
+
+        if (Icon) {
+          const iconElement = document.createElement("span");
+          iconElement.classList.add("icon");
+          iconElement.innerHTML = Icon;
+          dateCell.appendChild(iconElement);
         }
         break;
       }
@@ -74,6 +78,21 @@ const renderCalendar = () => {
 };
 renderCalendar();
 
+function getLatestIcon() {
+  var diaryEntries = loadDiaryEntries();
+
+  if (Object.keys(diaryEntries).length === 0) {
+    return null;
+  }
+
+  var currentDate = getCurrentDate();
+  if (diaryEntries[currentDate] && diaryEntries[currentDate].length > 0) {
+    var latestEntry =
+      diaryEntries[currentDate][diaryEntries[currentDate].length - 1];
+    return latestEntry.icon;
+  }
+  return null;
+}
 const prevMonth = () => {
   date.setDate(1);
   date.setMonth(date.getMonth() - 1);
@@ -89,3 +108,8 @@ const goToday = () => {
   date = new Date();
   renderCalendar();
 };
+
+function loadDiaryEntries() {
+  var diaryEntries = JSON.parse(localStorage.getItem("DiaryEntries")) || {};
+  return diaryEntries;
+}
